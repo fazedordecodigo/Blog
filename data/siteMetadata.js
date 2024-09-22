a648fcf89d0031e498906e53a51440b0873bf1ad
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2024-09-22 13:14:54 -0300

    Atualizado infos do blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,26 +1,23 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'fazedordecodigo.com',
+  author: 'Emerson Delatorre',
+  headerTitle: 'Fazedor de Código',
   description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  language: 'pt-br',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://fazedordecodigo.com',
+  siteRepo: 'https://github.com/fazedordecodigo/blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
-  // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
+  mastodon: 'https://hachyderm.io/@fazedordecodigo',
+  email: 'emerson@delatorre.dev',
+  github: 'https://github.com/fazedordecodigo',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/fazedordecodigo',
   threads: 'https://www.threads.net',
   instagram: 'https://www.instagram.com',
-  locale: 'en-US',
+  locale: 'pt-br',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
@@ -79,7 +76,7 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
       // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: 'pt-br',
     },
   },
   search: {
